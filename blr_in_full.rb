a17bf65c5ebea8eb531f339af0440b94733f59db
Gedean Dias <gedean.dias@gmail.com>
2020-06-25 18:33:58 -0300

    1ST COMMIT

---
+++ blr_in_full.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Entry point
+class BlrInFull
+  def initialize numeric
+
+  end
+
+  private
+
+  def decompose_in_classes(numeric)
+    whole_number = numeric.trunk
+  end
+end
+
